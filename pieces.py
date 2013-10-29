df707677c5e65828276bfb1fc51a4113b9fc1777
cannon10100 <cannon10100@gmail.com>
2013-10-28 21:18:53 -0500

    Create pieces.py

---
+++ pieces.py
@@ -0,0 +1,32 @@
+#!/bin/python
+
+class Piece:
+	def __init__(self, points = 0, name = "", location = (0,0)):
+		self.points = points
+		self.name = name
+		self.location = location
+		
+class Pawn(Piece):
+	def __init__(self):
+		Piece.__init__(self, 1, "pawn")
+
+class Bishop(Piece):
+	def __init__(self):
+		Piece.__init__(self, 3, "bishop")
+
+class Knight(Piece):
+	def __init__(self):
+		Piece.__init__(self, 3, "knight")
+
+class Rook(Piece):
+	def __init__(self):
+		Piece.__init__(self, 6, "rook")
+
+class Queen(Piece):
+	def __init__(self):
+		Piece.__init__(self, 9, "queen")
+
+class King(Piece):
+	def __init__(self):
+		Piece.__init__(self, 20, "king")
+		
