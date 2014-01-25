fb360a9bde0ffa86d1fa72a7de30799f6b12922f
Cannon Lewis <cannon10100@gmail.com>
2014-01-25 17:04:36 -0600

    Added evaluate method to Board class to sum up piece points.

--- board.py
+++ board.py
@@ -1,17 +1,30 @@
 from pieces import *
+from player import Player
 
 
 class Board:
     def __init__(self):
-        self.blocked = {}
+        self.pieces = {}
 
     def add_piece(self, piece):
         assert isinstance(piece, Piece), "The given object is not a piece"
-        self.blocked[piece.location] = piece
+        self.pieces[piece.location] = piece
 
     def check_tile(self, location):
         assert type(location) == tuple
-        if self.blocked.has_key(location):
+        if self.pieces.has_key(location):
             return True
         else:
             return False
+
+    def evaluate(self, player):
+        assert isinstance(player, Player), "Non-Player object passed in error"
+        point_sum = 0
+
+        for piece in self.pieces:
+            if piece.owner == player:
+                point_sum += piece.points
+            else:
+                point_sum -= piece.points
+
+        return point_sum
