7b1b752348a93c7484d8bdc5099238c1818c67c3
Cannon Lewis <cannon10100@gmail.com>
2014-02-07 22:49:14 -0600

    Switched the keys and values in the board.pieces dictionary so we can get pieces at certain locations. Implemented method to return a new board from a branch and a start board.

--- board.py
+++ board.py
@@ -11,11 +11,11 @@ class Board:
 
     def add_piece(self, piece):
         assert isinstance(piece, pieces.Piece), "The given object is not a piece"
-        self.pieces[piece] = piece.location
+        self.pieces[piece.location] = piece
 
     def check_tile_empty(self, location):
         assert type(location) == tuple
-        if location in self.pieces.values():
+        if location in self.pieces:
             return False
         else:
             return True
@@ -23,7 +23,7 @@ class Board:
     def evaluate(self, player_num):
         point_sum = 0
 
-        for piece in self.pieces:
+        for piece in self.pieces.values():
             if piece.player_num == player_num:
                 point_sum += piece.points
             else:
@@ -36,9 +36,11 @@ class Board:
 
         assert isinstance(current_branch, branch.Branch)
 
-        if current_branch.piece in self.pieces:
+        if current_branch.piece in self.pieces.values():
+            current_location = current_branch.piece.location
             current_branch.piece.location = current_branch.move_location
-            self.pieces[current_branch.piece] = current_branch.move_location
+            self.pieces[current_branch.move_location] = current_branch.piece
+            del(self.pieces[current_location])
 
     def create_branch_board(self, pos_branch):
         """Returns a new board with the branch applied to it"""
@@ -46,6 +48,10 @@ class Board:
         assert isinstance(pos_branch, branch.Branch)
 
         return_board = deepcopy(self)
-        return_board.apply_branch(pos_branch)
+
+        cur_piece = return_board.pieces[pos_branch.piece.location]
+        new_branch = branch.Branch(cur_piece, pos_branch.move_location)
+
+        return_board.apply_branch(new_branch)
 
         return return_board
