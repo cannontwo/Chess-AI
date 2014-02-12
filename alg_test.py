59db5c6a7888cc36b85d996bbd7c4c9e1aa463af
Cannon Lewis <cannon10100@gmail.com>
2014-02-12 12:13:32 -0600

    Changed alg_test to return nodes as a test of returning moves from the Chess-AI.

--- alg_test.py
+++ alg_test.py
@@ -2,8 +2,11 @@ import random
 
 
 class Node:
-    def __init__(self):
-        self.value = random.randint(0, 20)
+    def __init__(self, num=0):
+        if num == 0:
+            self.value = random.randint(0, 20)
+        else:
+            self.value = num
         self.child_num = random.randint(0, 5)
         self.children = []
 
@@ -13,21 +16,27 @@ class Node:
 
         return self.children
 
+    @staticmethod
+    def compare_node(node):
+        return node.value
+
 
 def alphabeta(node, depth, a, b, maximizing_player):
     if depth == 0 or node.child_num == 0:
-        return node.value
+        return node
     if maximizing_player:
         for child in node.generate_children():
-            a = max(a, alphabeta(child, depth - 1, a, b, False))
-            if b <= a:
+            a = max(a, alphabeta(child, depth - 1, a, b, False), key=Node.compare_node)
+            if b.value <= a.value:
                 break
         return a
     else:
         for child in node.generate_children():
-            b = min(b, alphabeta(child, depth - 1, a, b, True))
-            if b <= a:
+            b = min(b, alphabeta(child, depth - 1, a, b, True), key=Node.compare_node)
+            if b.value <= a.value:
                 break
         return b
 
-print "\nAnswer: " + str(alphabeta(Node(), 4, -99999999, 999999999, True))
+
+
+print "\nAnswer: " + str(alphabeta(Node(), 4, Node(-99999999), Node(999999999), True).value)
